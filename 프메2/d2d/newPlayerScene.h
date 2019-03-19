a49021f4d5987f6c8c5aa38099470de039d70715
921126 <nabom921126@gmail.com>
2019-03-19 19:47:56 +0900

    1231

--- 프메2/d2d/newPlayerScene.h
+++ 프메2/d2d/newPlayerScene.h
@@ -18,26 +18,29 @@ class newPlayerScene : public gameNode
 {
 private:
 	princess* _princess;
+
 	tagInfo _princessInfo;
 	tagStatus _princessStatus;
+	tagEtc _calendar[12][42];
+	tagEtc _fatherAge[10];
+	tagEtc _blood[4];
 
 	NEWPLAYER_STATE _nState;
 
 	string _fatherName;
 	string _daughterName;
 
-	tagEtc _calendar[12][42];
 	image* _img;
 
-	tagEtc _fatherAge[10];
 	list<int>			_lfatherAge;
 	list<int>::iterator _ilfatherAge;
 
 	int _fatherage;
 	int _fatherYear;
-
-	tagEtc _blood[4];
 	int _count;
+
+	D2D1_RECT_F _clearBtn;
+	D2D1_RECT_F _returnBtn;
 
 public:
 	newPlayerScene();
@@ -47,5 +50,8 @@ public:
 	void release();
 	void update();
 	void render();
+
+	void fatherNameCreat();
+	void dautherNameCreat();
 };
 
